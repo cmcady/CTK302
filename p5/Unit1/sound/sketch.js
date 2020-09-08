9f7d8938ccefd8a120f3d0108799048ec00bcde3
cmcady <58442593+cmcady@users.noreply.github.com>
2020-09-08 17:15:09 -0500

    sound

---
+++ p5/Unit1/sound/sketch.js
@@ -0,0 +1,25 @@
+var song1 ;
+
+
+function preload() {
+  song1 = loadSound("assets/guitarbeat.mp3") ;
+}
+
+function setup() {
+createCanvas(800, 800) ;
+song1.play() ;
+}
+
+
+
+function draw() {
+background(100);
+}
+
+function mouseReleased() {
+  if (song1.isPlaying()) {
+    song1.pause() ;
+  } else{
+    song1.loop() ; 
+  }
+}
